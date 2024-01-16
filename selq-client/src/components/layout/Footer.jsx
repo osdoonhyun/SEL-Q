05dafcb8727391d3f353badcc8c89e9e99e8bc12
osdoonhyun <skw1996@naver.com>
2024-01-16 18:12:49 +0900

    update: 2023->2024

--- selq-client/src/components/layout/Footer.jsx
+++ selq-client/src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { FooterDiv } from '../../styles/LayoutStyles';
 
 export default function Footer() {
-  return <FooterDiv>@ 2023. shinseunghyun all rights reserved.</FooterDiv>;
+  return <FooterDiv>@ 2024. shinseunghyun all rights reserved.</FooterDiv>;
 }
